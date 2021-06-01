aed85d62a6587e5891e9ed89553c7d67de6b3f24
Kain <drkain@outlook.com>
2021-06-01 12:24:08 +1200

    4 rules

--- data/rules.js
+++ data/rules.js
@@ -20,7 +20,7 @@ const $kurlc_rules = [
     {
         name: 'amazon.com',
         match: /www.amazon.com/i,
-        rules: ['ms3_c', 'pf_rd_s', 'pf_rd_t', ' pf_rd_i', 'pf_rd_m', 'pd_rd_w', 'qid', 'sr', 'keywords', 'dchild', 'ref', 'ref_', 'rnid', 'pf_rd_r', 'pf_rd_p', 'rh', 'pd_rd_r', 'smid', 'pd_rd_wg'],
+        rules: ['linkId', 'tag', 'linkCode', 'ms3_c', 'pf_rd_s', 'pf_rd_t', ' pf_rd_i', 'pf_rd_m', 'pd_rd_w', 'qid', 'sr', 'keywords', 'dchild', 'ref', 'ref_', 'rnid', 'pf_rd_r', 'pf_rd_p', 'rh', 'pd_rd_r', 'smid', 'pd_rd_wg'],
         replace: [/(\/ref|&ref_)=[^\/?]*/i]
     },
     {
@@ -158,7 +158,7 @@ const $kurlc_rules = [
     {
         name: 'linkedin.com',
         match: /.*.linkedin.com/i,
-        rules: ['contextUrn', 'destRedirectURL', 'lipi', 'trk', 'trkInfo', 'originalReferer', 'upsellOrderOrigin', 'upsellTrk', 'upsellTrackingId', 'src'],
+        rules: ['contextUrn', 'destRedirectURL', 'lipi', 'licu', 'trk', 'trkInfo', 'originalReferer', 'upsellOrderOrigin', 'upsellTrk', 'upsellTrackingId', 'src'],
         replace: []
     },
     {
