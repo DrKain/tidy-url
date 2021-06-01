ef81cbe488204a5a751dfc0606ae6bde78dcea5d
Kain <drkain@outlook.com>
2021-06-01 12:59:01 +1200

    1.0.14
    Better amazon regex and fixed a few rules

--- data/tidy.user.js
+++ data/tidy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tidy URL
 // @namespace    https://ksir.pw
-// @version      1.0.13
+// @version      1.0.14
 // @description  Cleans/removes garbage or tracking parameters from URLs
 // @author       Kain (ksir.pw)
 // @include      *
