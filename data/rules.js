f75d65b570bfbfea4d53d778e0cb81f001695794
Kain <drkain@outlook.com>
2022-07-04 18:15:54 +1200

    Expand global rules

--- data/rules.js
+++ data/rules.js
@@ -10,7 +10,9 @@ const $kurlc_rules = [
             'utm_pubreferrer', 'utm_swu', 'gclid', 'ga_source', 'ga_medium',
             'ga_term', 'ga_content', 'ga_campaign', 'ga_place', 'gclid', 'gclsrc',
             'itm_source', 'itm_medium', 'itm_term', 'itm_campaign', 'itm_content',
-            'itm_channel', 'itm_source_s', 'itm_medium_s', 'itm_campaign_s'
+            'itm_channel', 'itm_source_s', 'itm_medium_s', 'itm_campaign_s',
+            'fbclid', 'dclid', 'oly_enc_id', 'oly_anon_id', 'vero_id', '_hsenc',
+            'mkt_tok', 'mc_eid', 'utm_social-type', 'utm_brand'
         ]
     },
     {
