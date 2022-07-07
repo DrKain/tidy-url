8c7a8ec7c1aaa524499eed86f1d137103ab86318
Kain <drkain@outlook.com>
2022-07-08 01:42:21 +1200

    Update: gamesload.com
    REF is a valid parameter with the value referring to the game ID

--- data/rules.js
+++ data/rules.js
@@ -217,7 +217,7 @@ const $kurlc_rules = [
     {
         name: 'gamesload.com',
         match: /gamesload.com/i,
-        rules: ['affil', 'REF']
+        rules: ['affil']
     },
     {
         name: 'mightyape',
