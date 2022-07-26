8c9abc2a6e3f07f5b8198f46d946f5e2d5b05776
Kain <drkain@outlook.com>
2022-07-26 13:13:30 +1200

    1.5.5
    Fixed an issue where an incorrect redirect parameter would throw an error

--- data/tidy.user.js
+++ data/tidy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tidy URL
 // @namespace    https://ksir.pw
-// @version      1.5.4
+// @version      1.5.5
 // @description  Cleans/removes garbage or tracking parameters from URLs
 // @author       Kain (ksir.pw)
 // @include      *
