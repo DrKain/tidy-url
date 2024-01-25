f26456fa1c408e9f303a3a0f8071a1e0a2af825f
Kain <drkain@outlook.com>
2024-01-25 18:26:10 +1300

    1.10.5
    1 rule

--- data/tidy.user.js
+++ data/tidy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tidy URL
 // @namespace    https://ksir.pw
-// @version      1.10.4
+// @version      1.10.5
 // @description  Cleans/removes garbage or tracking parameters from URLs
 // @author       Kain (ksir.pw)
 // @include      *
