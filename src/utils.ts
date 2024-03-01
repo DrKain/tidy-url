4c9425d01796a5cc8698c50a6fadd3a079a228f2
Kain <drkain@outlook.com>
2024-03-02 02:52:19 +1300

    Move more useful functions to the utils file

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { IData, ILinkDiff } from './interface';
+
 /**
  * Accepts any base64 string and attempts to decode it.
  * If run through the browser `atob` will be used, otherwise
@@ -20,4 +22,70 @@ export const decodeBase64 = (str: string): string => {
     } catch (error) {
         return str;
     }
+};
+
+/**
+ * Checks if data is valid JSON. The result will be either `true` or `false`.
+ * @param data Any string that might be JSON
+ * @returns true or false
+ */
+export const isJSON = (data: string): boolean => {
+    try {
+        JSON.parse(data);
+        return true;
+    } catch (error) {
+        return false;
+    }
+};
+
+/**
+ * Check if a domain has any URL parameters
+ * @param url Any valid URL
+ * @returns true / false
+ */
+export const urlHasParams = (url: string): boolean => {
+    return new URL(url).searchParams.toString().length > 0;
+};
+
+/**
+ * Determine if the input is a valid URL or not. This will only
+ * accept http and https protocols.
+ * @param url Any URL
+ * @returns true / false
+ */
+export const validateURL = (url: string): boolean => {
+    try {
+        const pass = ['http:', 'https:'];
+        const test = new URL(url);
+        const prot = test.protocol.toLowerCase();
+
+        if (!pass.includes(prot)) {
+            throw new Error('Not acceptable protocol: ' + prot);
+        }
+
+        return true;
+    } catch (error) {
+        if (url !== 'undefined' && url !== 'null' && url.length > 0) {
+            throw new Error(`Invalid URL: ` + url);
+        }
+        return false;
+    }
+};
+
+/**
+ * Calculates the difference between two links and returns an object of information.
+ * @param firstURL Any valid URL
+ * @param secondURL Any valid URL
+ * @returns The difference between two links
+ */
+export const getLinkDiff = (firstURL: string, secondURL: string): ILinkDiff => {
+    const oldUrl = new URL(firstURL);
+    const newUrl = new URL(secondURL);
+
+    return {
+        is_new_host: oldUrl.host !== newUrl.host,
+        isNewHost: oldUrl.host !== newUrl.host,
+        difference: secondURL.length - firstURL.length,
+        reduction: +(100 - (firstURL.length / secondURL.length) * 100).toFixed(2)
+    };
 };
