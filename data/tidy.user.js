5facc8b2074820d88fb3ac7c89d3982aa59da846
Kain <drkain@outlook.com>
2024-05-05 14:19:41 +1200

    1.16.2
    - 2 rules
    - 1 new website
    - 1 updated website
    - 1 issue closed

--- data/tidy.user.js
+++ data/tidy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tidy URL
 // @namespace    https://ksir.pw
-// @version      1.16.1
+// @version      1.16.2
 // @description  Cleans/removes garbage or tracking parameters from URLs
 // @author       Kain (ksir.pw)
 // @include      *
