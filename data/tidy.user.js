3695a9103d6625dcaa581578e1cf0087b97dd10b
Kain <drkain@outlook.com>
2024-05-29 21:45:10 +1200

    Bump version

--- data/tidy.user.js
+++ data/tidy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tidy URL
 // @namespace    https://ksir.pw
-// @version      1.17.2
+// @version      1.17.3
 // @description  Cleans/removes garbage or tracking parameters from URLs
 // @author       Kain (ksir.pw)
 // @include      *
